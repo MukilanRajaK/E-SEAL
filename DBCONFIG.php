a6a2ebe2b7717751e421f02aec8deec3a4aaee4c
MukilanRajaK <kmukilanraja@gmail.com>
2020-08-26 08:54:14 +0530

    credentials removed

--- DBCONFIG.php
+++ DBCONFIG.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "id12165871_tamper");     // Your database user name
-define('DB_PASSWORD', "tamper");			// Your database password (mention your db password here)
-define('DB_DATABASE', "id12165871_tamper"); // Your database name
-define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+define('DB_USER', "");     // Your database user name
+define('DB_PASSWORD', "");			// Your database password (mention your db password here)
+define('DB_DATABASE', ""); // Your database name
+define('DB_SERVER', "");			// db server (Mostly will be 'local' host)
 ?>
